a7b0fffd701323b02dae02e6c2befb91463b0a6e
Rafael Ostertag <rafi@guengel.ch>
2013-01-08 20:22:31 +0000

    Added test for EVT_KEY. Not automated yet.

---
+++ tests/eventqueue9.cc
@@ -0,0 +1,88 @@
+// $Id: eventqueue8.cc 4620 2013-01-07 22:14:30Z rafisol $
+//
+// 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif // HAVE_UNISTD_H
+
+#ifdef HAVE_IOSTREAM
+#include <iostream>
+#endif // HAVE_IOSTREAM
+
+#ifdef HAVE_CSTDLIB
+#include <cstdlib>
+#endif // HAVE_CSTDLIB
+
+#include "curs.h"
+#include "eventconnector.h"
+#include "eventqueue.h"
+
+class Handler {
+    private:
+	EVENT_TYPE expected_evt;
+	int calls;
+    public:
+	inline Handler(EVENT_TYPE evt):
+	    expected_evt(evt), calls(0) {}
+	inline Handler(const Handler& _h) {
+	    expected_evt = _h.expected_evt;
+	    calls = _h.calls;
+	}
+	inline virtual ~Handler() {}
+	inline virtual int handler(EventBase& e) {
+	    if (e.type() != expected_evt) {
+		std::cerr << "Unexpected event type" << std::endl;
+		std::abort();
+	    }
+	    calls++;
+	    return 0;
+	}
+	inline int getCalls() { return calls; }
+	inline void reset() { calls=0; };
+};
+
+class MyHandler: public Handler {
+    private:
+	std::string _str;
+
+    public:
+	inline MyHandler(): Handler(EVT_KEY), _str() {}
+	inline int handler(EventBase& e) {
+	    Handler::handler(e);
+	    if (typeid(e) != typeid(EventKey)) std::abort();
+
+	    EventKey& tmp = dynamic_cast<EventKey&>(e);
+	    char c = static_cast<char>(tmp.data());
+	    _str.push_back(c);
+
+	    if (_str == "Hello, World!")
+	    	EventQueue::inject(EventBase(EVT_QUIT));
+	    return 0;
+	}
+};
+
+int main() {
+    try {
+	MyHandler h;
+	EventQueue::connectEvent(EventConnectorMethod1<MyHandler>(EVT_KEY, &h, &MyHandler::handler) );
+
+	Curses::init();
+
+	EventQueue::run();
+
+	Curses::end();
+    } catch (std::exception& e) {
+	std::cerr << e.what() << std::endl;
+	goto _ERR;
+    }
+
+    return 0;
+
+ _ERR:
+    Curses::end();
+    return 1;
+}

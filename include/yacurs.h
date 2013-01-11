15b120b8cb9a6e8daee48a76ee5990fe4e65d51e
Rafael Ostertag <rafi@guengel.ch>
2013-01-11 21:06:42 +0000

    All in one header file.

---
+++ include/yacurs.h
@@ -0,0 +1,23 @@
+// -*- mode: c++ -*-
+//
+// $Id$
+
+#ifndef YACURS_H
+#define YACURS_H
+
+#include "realizeable.h"
+#include "coordinates.h"
+#include "margin.h"
+#include "rectangle.h"
+#include "cursex.h"
+#include "event.h"
+#include "eventconnector.h"
+#include "eventqueue.h"
+#include "curs.h"
+#include "screenobject.h"
+#include "lineobject.h"
+#include "statusline.h"
+#include "window.h"
+
+#endif // YACURS_H
+

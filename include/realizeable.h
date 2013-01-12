0f59f2290da7c8e6fc31de0d45014c79df47ec1e
Rafael Ostertag <rafi@guengel.ch>
2013-01-12 21:14:11 +0000

    refresh() now takes argument indicating immediate or deferred update. realize() does not take an argument anymore.

--- include/realizeable.h
+++ include/realizeable.h
@@ -25,9 +25,12 @@ class Realizeable {
 
 	Realizeable& operator=(const Realizeable& r);
 	    
-	virtual void refresh() = 0;
+	virtual void refresh(bool immediate) = 0;
+	/**
+	 * Resizes the object. A resize must not refresh.
+	 */
 	virtual void resize(const Rectangle<>&) = 0;
-	virtual void realize(const Rectangle<>&) = 0;
+	virtual void realize() = 0;
 	bool isRealized() const;
 };
 

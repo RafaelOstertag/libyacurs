351e33c43f3a751a62dd5cc6f8eee7de2e19e29e
Rafael Ostertag <rafi@guengel.ch>
2013-01-12 20:53:35 +0000

    Now tests resize() method of Window

--- tests/resize1.cc
+++ tests/resize1.cc
@@ -29,94 +29,40 @@
 #include <iostream>
 #endif // HAVE_IOSTREAM
 
-#include "curs.h"
-#include "lineobject.h"
-#include "statusline.h"
-#include "window.h"
+#include "yacurs.h"
 
 int main() {
-    winsize ws, wsave;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1) {
-	return 1;
-    }
-
-    wsave = ws;
-
     try {
 	Curses::init();
 
-	LineObject* title = new LineObject(LineObject::POS_TOP,
-					   NULL, "Resize 1");
-	Curses::setTitle(title);
-
-	Window* w1 = new Window;
+	Window* w1 = new Window(Margin<>(1,0,1,0));
 	w1->setFrame(true);
 
-	Curses::setWindow(w1);
+	w1->realize();
+	w1->refresh(true);
 
-	StatusLine* sl = new StatusLine();
-	Curses::setStatusLine(sl);
+	sleep(1);
 
+	Rectangle<> scrsz(Curses::inquiryScreenSize());
+	Rectangle<> newsz(0,0,scrsz.rows()-1, scrsz.cols()-1);
+	w1->resize(newsz);
+	w1->refresh(true);
 
-	Curses::getStatusLine()->pushMsg("Status 1");
-	Curses::getStatusLine()->pushMsg("Status 2");
-	Curses::show();
+	sleep(1);
 
-	int line = ws.ws_row;
-	for (; ws.ws_col>10; ws.ws_col--) {
-	    for (ws.ws_row = line; ws.ws_row>10; ws.ws_row--) {
-		if (ioctl(STDIN_FILENO, TIOCSWINSZ, &ws) == -1) {
-		    return 1;
-		}
+	newsz=Rectangle<>(0,0,scrsz.rows()-2, scrsz.cols()-2);
+	w1->resize(newsz);
+	w1->refresh(true);
 
-		char buff[10];
-		sprintf(buff,"%d", ws.ws_col);
-		std::string tmp1 = buff;
-		sprintf(buff,"%d", ws.ws_row);
-		std::string tmp2 = buff;
+	sleep(1);
 
-		std::string msg = tmp1 + ":" + tmp2;
-		Curses::getStatusLine()->pushMsg(msg);
-	    }
-	}
-
-	line = ws.ws_row;
-	for (; ws.ws_col<=wsave.ws_col; ws.ws_col++) {
-	    for ( ws.ws_row = line; ws.ws_row<=wsave.ws_row; ws.ws_row++) {
-		if (ioctl(STDIN_FILENO, TIOCSWINSZ, &ws) == -1) {
-		    return 1;
-		}
-
-		char buff[10];
-		sprintf(buff,"%d", ws.ws_col);
-		std::string tmp1 = buff;
-		sprintf(buff,"%d", ws.ws_row);
-		std::string tmp2 = buff;
-
-		std::string msg = tmp1 + ":" + tmp2;
-		Curses::getStatusLine()->pushMsg(msg);
-	    }
-	}
-
-	delete title;
 	delete w1;
-	delete sl;
 	Curses::end();
     } catch (std::exception& e) {
+	Curses::end();
 	std::cerr << e.what() << std::endl;
-	sleep(2);
-	goto _ERR;
-    }
-
-    if (ioctl(STDIN_FILENO, TIOCSWINSZ, &wsave) == -1) {
 	return 1;
     }
 
     return 0;
- _ERR:
-    Curses::end();
-    if (ioctl(STDIN_FILENO, TIOCSWINSZ, &wsave) == -1) {
-	return 1;
-    }
-    return 1;
 }

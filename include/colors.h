f625d01d98b3a6d43de27605ebd67f052a217be0
Rafael Ostertag <rafi@guengel.ch>
2013-04-11 19:07:31 +0000

    Added copyright.

--- include/colors.h
+++ include/colors.h
@@ -1,27 +1,27 @@
-// -*- c++ -*-
-//
-// $Id$
-//
-// Copyright (C) 2008-2010  Rafael Ostertag
-//
-// This file is part of YAPET.
-//
-// YAPET is free software: you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or (at your option) any later
-// version.
-//
-// YAPET is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-// details.
-//
-// You should have received a copy of the GNU General Public License along with
-// YAPET.  If not, see <http://www.gnu.org/licenses/>.
-//
+/* -*- mode: c++ -*-
+ *
+ * This file is part of libyacurs, 
+ * Copyright (C) 2013  Rafael Ostertag
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 
-#ifndef _COLORS_H
-#define _COLORS_H
+// $Id$
+
+#ifndef COLORS_H
+#define COLORS_H
 
 #include <vector>
 #include <string>
@@ -182,4 +182,4 @@ namespace YACURS {
     };
 }
 
-#endif // _COLORS_H
+#endif // COLORS_H

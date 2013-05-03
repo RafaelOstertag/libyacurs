168a16276a5871cef2782500d0d01131c101b362
Rafael Ostertag <rafi@guengel.ch>
2013-05-03 09:28:53 +0000

    Updated copyright statement.

--- src/margin.cc
+++ src/margin.cc
@@ -1,5 +1,5 @@
 //
-// This file is part of libyacurs,
+// This file is part of libyacurs.
 // Copyright (C) 2013  Rafael Ostertag
 //
 // This program is free software: you can redistribute it and/or

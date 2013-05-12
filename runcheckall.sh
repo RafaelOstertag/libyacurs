87fb358246525eb1b05efc3ad2954c2102ff95b2
Rafael Ostertag <rafi@guengel.ch>
2013-05-12 11:55:09 +0000

    Will now launch an xterm for each host.

--- runcheckall.sh
+++ runcheckall.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-if [ -d ~/libyacurs-test ]
-then
-    cd ~/libyacurs-test
-    svn update || exit 1
-else
-    svn co svn+ssh://gizmo.kruemel.home/var/svn/swprojects/libyacurs/trunk ~/libyacurs-test || exit 1
-fi
-
-cd ~/libyacurs-test || exit 1
-make -f Makefile.boot || exit 1
-
 for h in fish dash debian32 freebsd32 openbsd32 netbsd32 abraxas aurora starchild odin
 do
-    ssh -t $h "cd $HOME/libyacurs-test ; if [ -x /usr/xpg4/bin/sh ] ; then /usr/xpg4/bin/sh checkall.sh ; else /bin/sh checkall.sh ; fi"
-    if [ $? -ne 0 ]
+    TESTDIR="${HOME}/libyacurs-test"
+    TESTDIRHOST="${TESTDIR}/${h}"
+
+    if [ ! -d "${TESTDIR}" ]
     then
-	echo "ERROR on $h"
-	exit 1
+	mkdir "${TESTDIR}"
     fi
+
+    if [ -d "${TESTDIRHOST}" ]
+    then
+	cd "${TESTDIRHOST}"
+	svn update || exit 1
+    else
+	svn co svn+ssh://gizmo.kruemel.home/var/svn/swprojects/libyacurs/trunk "${TESTDIRHOST}" || exit 1
+    fi
+    
+    cd "${TESTDIRHOST}" || exit 1
+    make -f Makefile.boot || exit 1
+
+    xterm -T "${h}" -n "${h}" -e "ssh -t $h 'cd ${TESTDIRHOST} ; if [ -x /usr/xpg4/bin/sh ] ; then /usr/xpg4/bin/sh checkall.sh ; else /bin/sh checkall.sh ; fi ; read BLA'" &
 done
 
 exit 0

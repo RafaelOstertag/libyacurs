03484e0a059902fe07208adaa5ccc660f07d3f91
Rafael Ostertag <rafi@guengel.ch>
2013-05-21 18:32:23 +0000

    Added more tests.

--- tests/curswin2.cc
+++ tests/curswin2.cc
@@ -16,6 +16,48 @@
 
 #include "yacurs.h"
 
+void test1() {
+   YACURS::INTERNAL::CursWin win(YACURS::Area(0, 10, 10, 10) );
+    win.box();
+
+    YACURS::CurStr str("ÄÖÜËäöüë", YACURS::Coordinates(1,1));
+    win.addstr(str);
+
+    win.refresh();
+}
+
+void test2() {
+    YACURS::INTERNAL::CursWin win(YACURS::Area(0, 10, 15, 15) );
+    win.box();
+
+    YACURS::CurStr str("ÄÖÜËäöüë", YACURS::Coordinates(5,5));
+    win.addstr(str);
+
+    win.refresh();
+}
+
+void test3() {
+    YACURS::INTERNAL::CursWin win(YACURS::Area(0, 10, 5, 5) );
+    win.box();
+
+    YACURS::CurStr str("ÄÖÜËäöüë", YACURS::Coordinates(2,2));
+    win.addstrx(str);
+
+    win.refresh();
+}
+
+void test4() {
+    YACURS::INTERNAL::CursWin win(YACURS::Area(0, 10, 12, 12) );
+    win.box();
+
+    YACURS::CurStr str("ÄÖÜËäöüë", YACURS::Coordinates(0,1));
+    win.addlinex(str);
+
+    YACURS::CurStr str1("ÄÖÜËäöüë", YACURS::Coordinates(0,2), YACURS::DIALOG_TITLE);
+    win.addlinex(str1);
+
+    win.refresh();
+}
 
 int
 main() {
@@ -27,17 +69,35 @@ main() {
     setlocale(LC_ALL, "");
 
     YACURS::Curses::init();
+    
+    test1();
 
-    YACURS::INTERNAL::CursWin win(YACURS::Area(0, 10, 10, 10) );
-    win.box();
+    sleep(3);
+    
+    wclear(stdscr);
+    wrefresh(stdscr);
 
-    YACURS::CurStr str("ÄÖÜËäöüë", YACURS::Coordinates(1,1));
-    win.addstr(str);
+    test2();
 
-    win.refresh();
+    sleep(3);
 
-    sleep(5);
+    wclear(stdscr);
+    wrefresh(stdscr);
 
+    test3();
+
+    sleep(3);
+
+    wclear(stdscr);
+    wrefresh(stdscr);
+
+    test4();
+
+    sleep(3);
+
+    wclear(stdscr);
+    wrefresh(stdscr);
+ 
     YACURS::Curses::end();
 
     return 0;

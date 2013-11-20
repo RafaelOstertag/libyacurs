d515403e2e3bf6d7f89367a1b00bb856e167f67d
Rafael Ostertag <rafi@guengel.ch>
2013-11-20 10:15:35 +0000

    use autreconf. added puffy. use http for svn.

--- runcheckall.sh
+++ runcheckall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for h in fish dash debian32 freebsd32 openbsd32 netbsd32 abraxas aurora starchild odin wheezy
+for h in fish dash debian32 freebsd32 openbsd32 netbsd32 abraxas aurora starchild odin wheezy puffy
 do
     TESTDIR="${HOME}/libyacurs-test"
     TESTDIRHOST="${TESTDIR}/${h}"
@@ -15,11 +15,13 @@ do
 	cd "${TESTDIRHOST}"
 	svn update || exit 1
     else
-	svn co svn+ssh://gizmo.kruemel.home/var/svn/swprojects/libyacurs/trunk "${TESTDIRHOST}" || exit 1
+	svn co https://gizmo.kruemel.home/svn/swprojects/libyacurs/trunk "${TESTDIRHOST}" || exit 1
     fi
     
     cd "${TESTDIRHOST}" || exit 1
-    make -f Makefile.boot || exit 1
+    touch ChangeLog
+    touch README
+    autoreconf -fi || exit 1
 
     xterm -T "${h}" -n "${h}" -e "ssh -t $h 'cd ${TESTDIRHOST} ; if [ -x /usr/xpg4/bin/sh ] ; then /usr/xpg4/bin/sh checkall.sh ; else /bin/sh checkall.sh ; fi ; read BLA'" &
 done

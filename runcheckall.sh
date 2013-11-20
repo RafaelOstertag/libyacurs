b3c43a8901b6526243a3bf27dfd4414d53f461c4
Rafael Ostertag <rafi@guengel.ch>
2013-11-20 12:27:44 +0000

    removed return code output. gave trouble with nested `"' and parantheses.

--- runcheckall.sh
+++ runcheckall.sh
@@ -23,7 +23,7 @@ do
     touch README
     autoreconf -fi || exit 1
 
-    xterm -T "${h}" -n "${h}" -e "ssh -t $h 'cd ${TESTDIRHOST} ; if [ -x /usr/xpg4/bin/sh ] ; then /usr/xpg4/bin/sh checkall.sh ; else /bin/sh checkall.sh ; fi ; echo "============= DONE \($?\) =============" ; read BLA'" &
+    xterm -T "${h}" -n "${h}" -e "ssh -t $h 'cd ${TESTDIRHOST} ; if [ -x /usr/xpg4/bin/sh ] ; then /usr/xpg4/bin/sh checkall.sh ; else /bin/sh checkall.sh ; fi ; echo '============= DONE =============' ; read BLA'" &
 done
 
 exit 0
